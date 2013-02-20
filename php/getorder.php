9289f172f8082a16f1f6942a0ac818c1cc9edb9e
projetbip <projetn7bip@gmail.com>
2013-02-20 09:45:34 +0100

    Correction lors de la réception de commandes et ajout d'un reset des commandes

--- php/getorder.php
+++ php/getorder.php
@@ -15,13 +15,20 @@ if (isset($_GET["target"])){
 if (isset($_GET["seq"])){
 	$seq = $_GET["seq"];
 }else{
-	$seq = 0;
+	$nb_cmd_query = mysqli_query($link,"SELECT COUNT(*) FROM Command WHERE ip = '$robot_ip'");
+	$nb_cmd = mysqli_fetch_row($nb_cmd_query);
+	echo $nb_cmd[0];
+	exit();
+}
+if (isset($_GET["reset"])){
+	mysqli_query($link,"DELETE FROM Command WHERE ip='$robot_ip'");
+	exit();
 }
 // get order id
-$order_result = mysqli_query($link,"SELECT ct.id_nxc FROM Command c, CommandType ct WHERE c.ip = '$robot_ip' AND ct.action = c.action");
+$order_result = mysqli_query($link,"SELECT ct.id_nxc FROM Command c, CommandType ct WHERE c.ip = '$robot_ip' AND ct.action = c.action AND seq='$seq'");
 $order = mysqli_fetch_array($order_result,MYSQLI_ASSOC);
 // get parameters of the command
-$param_result = mysqli_query($link,"SELECT label,value FROM Parameter WHERE ip = '$robot_ip' AND seq=$seq");
+$param_result = mysqli_query($link,"SELECT label,value FROM Parameter WHERE ip = '$robot_ip' AND seq='$seq'");
 
 // print the result
 echo $order["id_nxc"];
